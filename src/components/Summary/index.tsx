d898a4c0a297b5065aa1e1d7e4daa944dd20fc9b
Gabriel Luiz <gabrielluiz3011@gmail.com>
2021-03-25 15:17:46 -0300

    first commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import { Container } from './styles';
+import incomeImg from '../../assets/entradas.svg';
+import outcomeImg from '../../assets/saidas.svg';
+import totalImg from '../../assets/total.svg';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
+
+export function Summary() {
+  const transactions = useContext(TransactionsContext);
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Entradas</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
